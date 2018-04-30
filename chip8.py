3637e982c3aa3470e242ab1b7a97613aedc04b27
Mikel Bober-Irizar <mikel@mxbi.net>
2018-04-30 19:43:07 +0100

    Add more opcodes

--- chip8.py
+++ chip8.py
@@ -17,15 +17,26 @@ class Chip8:
 
         self.clock_speed = 512
         self.clock = 0
-        self.rtc_mod = 60 // self.clock_speed
+        self.rtc_mod = self.clock_speed // 60
 
         self.load_rom(rom)
 
         self.instructions = {
+            0x1000: self.op_1NNN,
+            0x2000: self.op_2NNN,
+            0x3000: self.op_3XNN,
+            0x4000: self.op_4XNN,
+            0x5000: self.op_5XY0,
+            0x6000: self.op_6XNN,
+            0x7000: self.op_7XNN,
+            0x8000: self.op_8000,
             0xA000: self.op_ANNN,
-            0xC000: self.op_CNNN,
+            0xB000: self.op_BNNN,
+            0xC000: self.op_CXNN,
+            0xD000: self.op_DXYN,
         }
 
+    # Emulate overflow and underflow
     def _8bit(self, val):
         return val % 256
 
@@ -51,14 +62,15 @@ class Chip8:
 
         # Read two bytes from memory, little endian
         opcode = self.ram[self.pc] << 8 | self.ram[self.pc + 1]
+        print('0x{:04X}'.format(opcode))
 
-        self.arch[opcode & 0xF000](opcode) # Mask opcode, get associated function, and execute
+        self.instructions[opcode & 0xF000](opcode) # Mask opcode, get associated function, and execute
         self.pc += 2 # Increment program counter to next instruction
 
         if self.clock % self.rtc_mod == 0:
-            if self.r_delay > 0:
+            if self.t_delay > 0:
                 self.t_delay -= 1
-            if self.r_sound > 0:
+            if self.t_sound > 0:
                 self.t_sound -= 1
 
     # Render to the display in text format to a string
@@ -81,34 +93,69 @@ class Chip8:
                 byte = f.read(1)
                 if not byte:
                     break
-                self.ram[ptr] = int.from_bytes(byte, 'little')
+                self.ram[ptr] = int.from_bytes(byte, 'big')
                 ptr += 1
 
     ##### INSTRUCTION SET #####
     # https://en.wikipedia.org/wiki/CHIP-8#Opcode_table for more details
+    def op_1NNN(self, opcode):  # Goto NNN
+        self.pc = opcode & 0x0FFF
+
     def op_2NNN(self, opcode):  # Call subroutine
         self.stack[self.sp] = self.pc # Place current location on the stack so we can go back to it later
         self.pc = opcode & 0x0FFF
 
     def op_3XNN(self, opcode):  # Skip next instruction if Vx == NN
-        Vx = self.V[opcode & 0x0F00]
+        Vx = self.V[(opcode & 0x0F00) >> 8]
         if Vx == (opcode & 0x00FF):
             self.pc += 2  # Skip next instruction
 
     def op_4XNN(self, opcode):  # Skip next instruction if Vx != NN
-        Vx = self.V[opcode & 0x0F00]
+        Vx = self.V[(opcode & 0x0F00) >> 8]
         if Vx != (opcode & 0x00FF):
             self.pc += 2
 
     def op_5XY0(self, opcode):  # Skip next instruction if Vx == Vy
-        Vx = self.V[opcode & 0x0F00]
-        Vy = self.V[opcode & 0x00F0]
+        Vx = self.V[(opcode & 0x0F00) >> 8]
+        Vy = self.V[(opcode & 0x00F0) >> 4]
         if Vx == Vy:
             self.pc += 2
 
+    def op_6XNN(self, opcode):  # Set Vx to NN
+        self.V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF)
+
+    def op_7XNN(self, opcode):  # Increment Vx by NN
+        self.V[(opcode & 0x0F00) >> 8] = self._8bit(self.V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF))
+
+    # Determine which 8*** opcode to execute by masking off 8XY*
+    def op_8000(self, opcode):
+        {0x8000: self.op_8XY0, 0x8001: self.op_8XY1, 0x8002: self.op_8XY2, 0x8003: self.op_8XY3, 0x8004: self.op_8XY4, 0x8005: self.op_8XY5}[opcode & 0xF00F](opcode)
+
+    def op_8XY0(self, opcode):  # Set Vx to value of Vy
+        self.V[(opcode & 0x0F00) >> 8] = self.V[(opcode & 0x00F0) >> 4]
+
+    def op_8XY1(self, opcode):  # Bitwise OR, Vx=Vx|Vy
+        self.V[(opcode & 0x0F00) >> 8] = self.V[(opcode & 0x0F00) >> 8] | self.V[(opcode & 0x00F0) >> 4]
+
+    def op_8XY2(self, opcode):  # Bitwise AND, Vx=Vx&Vy
+        self.V[(opcode & 0x00F0) >> 4] = self.V[(opcode & 0x0F00) >> 8] & self.V[(opcode & 0x00F0) >> 4]
+
+    def op_8XY3(self, opcode):  # Bitwise XOR, Vx=Vx&Vy
+        self.V[(opcode & 0x00F0) >> 4] = self.V[(opcode & 0x0F00) >> 8] ^ self.V[(opcode & 0x00F0) >> 4]
+
+    def op_8XY4(self, opcode):  # Vx = Vx + Vy, set VF to 1 if overflow
+        sum = self.V[(opcode & 0x0F00) >> 8] + self.V[(opcode & 0x00F0) >> 4]
+        self.V[0xF] = int(sum > 0xFFFF)
+        self.V[(opcode & 0x0F00) >> 8] = self._8bit(sum)
+
+    def op_8XY5(self, opcode):  # Vx = Vy - Vx, set VF to 1 if NO underflow
+        sub = self.V[(opcode & 0x0F00) >> 8] + self.V[(opcode & 0x00F0) >> 4]
+        self.V[0xF] = int(sub > 0x0000)
+        self.V[(opcode & 0x0F00) >> 8] = self._8bit(sum)
+
     def op_ANNN(self, opcode): self.I = (opcode & 0x0FFF)  # Set I to NNN
     def op_BNNN(self, opcode): self.pc = self.V[0] + (opcode & 0x0FFF) # Not really sure what this is for
-    def op_CXNN(self, opcode): self.V[opcode & 0x0F00] = random.randint(0, 255) & (opcode & 0x00FF) # Random number and mask with NN
+    def op_CXNN(self, opcode): self.V[(opcode & 0x0F00) >> 8] = random.randint(0, 255) & (opcode & 0x00FF) # Random number and mask with NN
 
     # Draw a sprite starting at (X, Y) with width 8 and height N, using bits starting from address I.
     def op_DXYN(self, opcode):
@@ -117,10 +164,13 @@ class Chip8:
         h = opcode & 0x000F
         ptr = self.I
         for y_offset in range(h):
-            row = [int(i) for i in '{0:08b}'.format(self.mem[ptr])] # Get 8 bits as separate integers
+            row = [int(i) for i in '{0:08b}'.format(self.ram[ptr])] # Get 8 bits as separate integers
             y = y0 + y_offset
             self.display[y * 64 + x0:y + 64 + x0 + 8] = row
             ptr += 1
 
+
 cpu = Chip8('pong.rom')
 cpu.print_mem(cpu.ram)
+while True:
+    cpu._cycle_clock()
